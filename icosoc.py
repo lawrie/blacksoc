f77aa46868ae11f883c032e31b3621351b6bf90f
lawrie <lawrie.griffiths@ntlworld.com>
2019-11-10 20:06:38 +0000

    Changed cfg files for blackicemx

--- icosoc.py
+++ icosoc.py
@@ -93,7 +93,7 @@ def make_pins(pname):
         return make_pins(pname + "_10") + make_pins(pname + "_9") + make_pins(pname + "_8") + make_pins(pname + "_7") + \
                make_pins(pname + "_4") + make_pins(pname + "_3") + make_pins(pname + "_2") + make_pins(pname + "_1")
 
-    if re.match(r"^[A-Z][0-9][0-9]?$", pname):
+    if re.match(r"^[A-Z][0-9][0-9][0-9]?$", pname):
         ploc = pname[1:]
 
     assert ploc is not None
